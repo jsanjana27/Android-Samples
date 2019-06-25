2d74859fa411ba150fcae03f3af4937e9ff25e4b
jsanjana27 <jsanjana96@gmail.com>
2019-06-25 12:50:54 +0530

    Using google maps API

---
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/ExampleFragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmentsample;
+
+import android.support.v4.app.Fragment;
+
+public class ExampleFragment extends Fragment {
+}
---
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fragmentsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
