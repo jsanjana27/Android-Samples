986b876631b393e8a059ea16defeac08c28defe0
jsanjana27 <jsanjana96@gmail.com>
2019-06-25 15:56:09 +0530

    Getting last location

--- MapsSample/app/src/main/java/com/example/mapssample/MapsActivity.java
+++ MapsSample/app/src/main/java/com/example/mapssample/MapsActivity.java
@@ -1,18 +1,33 @@
 package com.example.mapssample;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-import android.os.Bundle;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private double latitude;
+    private double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +37,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        } else {
+            getLocation();
+        }
     }
 
+    @SuppressLint("MissingPermission")
+    public void getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.d("MM", "onSuccess() called with: location = [" + location.getLatitude() + " " + location.getLongitude() + "]");
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            LatLng myLocation = new LatLng(latitude, longitude);
+                            mMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+                        }
+                    }
+                });
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +80,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng bangalore = new LatLng(12.972442, 77.580643);
-        mMap.addMarker(new MarkerOptions().position(bangalore).title("Marker in Bangalore"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bangalore));
+//         Add a marker in Sydney and move the camera
+//        LatLng myLocation = new LatLng(latitude, longitude);
+//        Log.d("MM", "Location called with: location = [" + latitude + " " + longitude + "]");
+//        mMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                }
+                return;
+            }
+        }
     }
 }
