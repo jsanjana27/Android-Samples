dfc23cdf036e6500c41abeb915e0010f696bbc2d
jsanjana27 <jsanjana96@gmail.com>
2019-06-26 14:22:16 +0530

    Broadcasting example

---
+++ BroadcastSample/app/src/main/java/com/example/broadcastsample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.broadcastsample;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BroadcastReceiver br = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
+    }
+}
