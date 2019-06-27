c62f211bc987bcecff3afef089f7db45be2c5cec
jsanjana27 <jsanjana96@gmail.com>
2019-06-27 12:57:20 +0530

    Using broadcast

--- BroadcastSample/app/src/main/java/com/example/broadcastsample/MainActivity.java
+++ BroadcastSample/app/src/main/java/com/example/broadcastsample/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.broadcastsample;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +19,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BroadcastReceiver br = new MyBroadcastReceiver();
-        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-        this.registerReceiver(br, filter);
+        IntentFilter filter = new IntentFilter("study");
+//        filter.addAction("study");
+        registerReceiver(br, filter);
+
+        Log.d("MM", "onCreate: ");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = new Intent();
+        intent.setAction("study");
+        intent.putExtra("name", "Sanjana");
+
+        sendBroadcast(intent);
+//        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+        private static final String TAG = "MyBroadcastReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.d(TAG, "onReceive() called with: context = [" + context + "], intent = [" + intent + "]");
+
+            if (intent.hasExtra("name")) {
+                Log.d(TAG, "onReceive: name " + intent.getStringExtra("name"));
+            }
+        }
     }
 }
--- BroadcastSample/app/src/main/java/com/example/broadcastsample/MyBroadcastReceiver.java
+++ BroadcastSample/app/src/main/java/com/example/broadcastsample/MyBroadcastReceiver.java
@@ -12,11 +12,12 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Action: " + intent.getAction() + "\n");
-        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
-        String log = sb.toString();
-        Log.d(TAG, log);
-        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("Action: " + intent.getAction() + "\n");
+//        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+//        String log = sb.toString();
+//        Log.d(TAG, log);
+//        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
     }
 }
+
